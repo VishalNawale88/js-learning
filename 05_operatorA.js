d08d46b9e8aa1db0952cbc96c701e41215b36194
VishalNawale88 <160692404+VishalNawale88@users.noreply.github.com>
2024-03-14 22:26:06 +0530

    Operator Assignment
    Operator Assignment

---
+++ 05_operatorA.js
@@ -0,0 +1,55 @@
+
+console.log("======== Step = 1 ================");
+//1.Function with arguments and no return value
+function squareOfWordLength(greet){
+    var greet = "JavaScript";
+    var totalChars = greet.length;
+    //console.log("Total number of chars is:" , totalChars);
+}
+squareOfWordLength();
+var greet = 10;
+var result = greet * greet;
+console.log(`Length Square Of JavaScript: ${result}`);
+
+
+//1.Function with arguments and no return value
+function squareOfWordLength(greet){
+    var greet = "Google Chrome";
+    var totalChars = greet.length;
+    //console.log("Total number of chars is:" , totalChars);
+}
+squareOfWordLength();
+var greet = 13;
+var result = greet * greet;
+console.log(`Length Square Of Google Chrome is: ${result}`);
+
+
+//1.Function with arguments and no return value
+function squareOfWordLength(greet){
+    var greet = "Developer Smart";
+    var totalChars = greet.length;
+    //console.log("Total number of chars is:" , totalChars);
+}
+squareOfWordLength();
+var greet = 15;
+var result = greet * greet;
+console.log(`Length Square Of Developer Smart is: ${result}`);
+
+
+console.log("======== Step = 2 ================");
+//2. Function with no arguments and no return value (Normal Function)
+function show(){
+    var word = "I am Angular Developer";
+    var totalChars = word.length;
+    //console.log("Total number of chars is:" , totalChars);
+}
+show();
+var n1 = 22;
+var n2 = 4; 
+var result = n1 / n2; 
+console.log(`String length after division is: ${result}`);
+
+var n1 = 22;
+var n2 = 4; 
+var result = n1 * n2;
+console.log(`String length after multiplication is: ${result}`);
