9cb3e579138cd008fbfee0ee0cff146a763b3c0e
VishalNawale88 <160692404+VishalNawale88@users.noreply.github.com>
2024-04-15 21:58:02 +0530

    Assignment
    Assignment

--- 28_domAssign.js
+++ 28_domAssign.js
@@ -3,16 +3,16 @@
 //console.log(elementProfile.innerHTML);
 
  
-console.log(`====== Step = 1 Answer =======`);
+console.log(`====== Step = 1 Answer ========`);
 const elementH1 = document.querySelector('h1');
 console.log(elementH1);
 console.log(elementH1.innerHTML);
 
-console.log(`====== Step = 2 Answer =======`);
+console.log(`====== Step = 2 Answer ========`);
 const elementUI = document.querySelector('.ui');
 console.log(elementUI.innerHTML);
 
-console.log(`======= Step = 3 Answer ========`);
+console.log(`======= Step = 3 Answer =========`);
 const elementProfile = document.getElementById("advantages");
 console.log(elementProfile);
 console.log(elementProfile.innerHTML);
